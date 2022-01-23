bbdbd69331024cfae829cd4a5a72a79df2473658
syket <syketweb@gmail.com>
2022-01-23 23:34:46 +0600

    react quill added

--- components/SubPages/NewStory/index.tsx
+++ components/SubPages/NewStory/index.tsx
@@ -1,11 +1,56 @@
-import React, { FC } from 'react';
+import React, { useState, FC } from 'react';
+import dynamic from 'next/dynamic';
+const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
+import 'react-quill/dist/quill.snow.css';
 
 const SubNewStory: FC = () => {
+  const [details, setDetails] = useState<string>('');
+
+  const handleBodyChange = (e: any) => {
+    setDetails(e);
+  };
+
   return (
     <div className="container">
       <h1>New Story</h1>
+      <ReactQuill
+        modules={modules}
+        formats={formats}
+        value={details}
+        onChange={(e) => handleBodyChange(e)}
+        placeholder="Write something amazing..."
+      />
     </div>
   );
 };
+
+const modules = {
+  toolbar: [
+    [{ header: '1' }, { header: '2' }, { header: [3, 4, 5, 6] }, { font: [] }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['link', 'image', 'video'],
+    ['clean'],
+    ['code-block'],
+  ],
+};
+
+const formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'link',
+  'image',
+  'video',
+  'code-block',
+];
 
 export default SubNewStory;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import '../styles/globals.css';
-import 'draft-js/dist/Draft.css';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
