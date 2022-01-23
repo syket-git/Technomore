6ab73989b2e9c2071a3d04831a05e7d3cb573124
syket <syketweb@gmail.com>
2022-01-23 10:53:22 +0600

    new changes

--- components/SubPages/Signup/index.tsx
+++ components/SubPages/Signup/index.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
 import Input from '../../Forms/Input/Input';
 import styles from './Signup.module.css';
+import Link from 'next/link';
 
 const SubSignupPages: FC = () => {
   return (
     <div className={styles.signup__wrapper}>
-      <h1 className="text-4xl text-secondary font-bold text-center">Signup</h1>
+      <h1 className="text-4xl mt-5 text-secondary font-bold text-center">
+        Signup
+      </h1>
       <div className="mt-8 mb-16 text-center">
         <form className="space-y-5">
           <Input placeholder={'Full Name'} type={'text'} />
@@ -15,6 +18,14 @@ const SubSignupPages: FC = () => {
           <Input type={'file'} />
           <Input value={'Submit'} type={'submit'} />
         </form>
+        <p className="py-2">
+          Already have an account?{' '}
+          <Link passHref href="/login">
+            <span className="font-medium underline cursor-pointer">
+              Sign in
+            </span>
+          </Link>
+        </p>
       </div>
     </div>
   );
